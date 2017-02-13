415d1ca972904fdc57ae01b674ca6664edd828f8
DVigneault <davis.vigneault@gmail.com>
2017-02-13 17:47:44 +0000

    Clean up insertion sort

--- src/include/dvInsertionSort.h
+++ src/include/dvInsertionSort.h
@@ -49,49 +49,6 @@ void InsertionSort(const It b, const It e)
 
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//template<typename T>
-//void InsertionSort(std::vector<T> &v)
-//{
-//  for (std::size_t i = 1; i < v.size(); ++i)
-//    {
-//    T temp = v[i];
-//    int j = i - 1;
-//    for (; (j >= 0 && v[j] > temp); --j)
-//      {
-//      v[j+1] = v[j];
-//      }
-//    v[j+1] = temp;
-//    }
-//}
 }
 
 #endif
